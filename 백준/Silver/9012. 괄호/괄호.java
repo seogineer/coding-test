f40ff0add56151bddc21b17f325225f413a88d8c
DoGyeong Seo <dgseo8981@gmail.com>
2023-12-06 19:12:43 +0900

    [Silver IV] Title: 괄호, Time: 232 ms, Memory: 17884 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,37 @@
 import java.util.*;
 class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int tc = Integer.parseInt(sc.nextLine());
-    for(int t = 0; t < tc; t++){
-      if(isValidParenthesisString(sc.nextLine())){
-        System.out.println("YES");
-        continue;
-      }
-      System.out.println("NO");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for(int t = 0; t < T; t++){
+            boolean flag = isValid(sc.next());
+            if(flag){
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+        sc.close();
     }
-  }
 
-  private static boolean isValidParenthesisString(String input){
-    Stack<Character> stack = new Stack<>();
-    for(int i = 0; i < input.length(); i++){
-      if(input.charAt(i) == '('){
-        stack.push(input.charAt(i));
-        continue;
-      }
-      if(input.charAt(i) == ')' && !stack.isEmpty()){
-        stack.pop();
-        continue;
-      }
-      if(input.charAt(i) == ')' && stack.isEmpty()){
-        return false;
-      }
+    public static boolean isValid(String input){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < input.length(); i++){
+            if(input.charAt(i) == '('){
+                stack.push(input.charAt(i));
+                continue;
+            } else if(input.charAt(i) == ')' && !stack.isEmpty()){
+                stack.pop();
+                continue;
+            } else if(input.charAt(i) == ')' && stack.isEmpty()){
+                return false;
+            }
+        }
+
+        if(stack.isEmpty()){
+            return true;
+        } else {
+            return false;
+        }
     }
-    
-    if(stack.isEmpty()){
-      return true;
-    }
-    return false;
-  }
 }
