9d49f8b4430d78737aaf95d9cba5d66a81533ebf
DoGyeong Seo <dgseo8981@gmail.com>
2023-12-13 19:49:14 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 672 ms, Memory: 297960 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,25 +1,35 @@
 import java.util.*;
 class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    Queue<String> queue = new LinkedList<>();
-    for(int i = 1; i <= N; i++){
-      queue.offer(String.valueOf(i));
-    }
-
-    List<String> list = new ArrayList<>();
-    int K = sc.nextInt();
-    for(int t = 0; t < N; t++){
-      for(int i = 1; i <= K; i++){
-        if(i == K){
-          list.add(queue.poll());
-          break;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> q = new LinkedList<>();
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        
+        for(int n = 1; n <= N; n++){
+            q.add(n);
         }
-        queue.offer(queue.poll());
-      }
-    }
 
-    System.out.println("<" + String.join(", ", list) + ">");
-  }
+        List<Integer> list = new ArrayList<>();
+        while(!q.isEmpty()){
+            for(int i = 1; i < K; i++){
+                q.add(q.poll());    
+            }
+            list.add(q.poll());
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        for(int i = 0; i < list.size(); i++){
+            if(i == list.size() - 1){
+                sb.append(list.get(i));
+                continue;
+            }
+            sb.append(list.get(i)).append(", ");
+        }
+        sb.append(">");
+        
+        System.out.println(sb.toString());
+        sc.close();
+    }
 }
