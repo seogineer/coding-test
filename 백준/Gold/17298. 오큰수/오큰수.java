f4212f9eae75585c528be3815b12cb34835436aa
DoGyeong Seo <dgseo8981@gmail.com>
2023-12-20 20:33:59 +0900

    [Gold IV] Title: 오큰수, Time: 2172 ms, Memory: 322200 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -2,31 +2,31 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-
-    int tc = Integer.parseInt(sc.nextLine());
-    int[] input = new int[tc];
-    for(int i = 0; i < tc; i++){
-      input[i] = sc.nextInt();
+    
+    int N = sc.nextInt();
+    int[] sequence = new int[N];
+    for(int i = 0; i < sequence.length; i++){
+      sequence[i] = sc.nextInt();
     }
 
     Stack<Integer> stack = new Stack<>();
-    for(int i = 0; i < tc; i++){
-      while(!stack.isEmpty() && input[stack.peek()] < input[i]){
-        input[stack.pop()] = input[i];
+    for(int i = 0; i < sequence.length; i++){
+      while(!stack.isEmpty() && sequence[stack.peek()] < sequence[i]){
+        sequence[stack.pop()] = sequence[i];
       }
-      
       stack.push(i);
     }
 
     while(!stack.isEmpty()){
-      input[stack.pop()] = -1;
+      sequence[stack.pop()] = -1;
     }
 
     StringBuilder sb = new StringBuilder();
-    for(int el : input){
-       sb.append(el).append(' ');
+    for(int i = 0; i < sequence.length; i++){
+      sb.append(sequence[i]).append(' ');
     }
-
-    System.out.println(sb);
+    System.out.println(sb.toString());
+    
+    sc.close();
   }
 }
