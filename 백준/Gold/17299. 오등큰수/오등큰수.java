95b20e780357240bdbb69b771f15d1be8b6d4d10
DoGyeong Seo <dgseo8981@gmail.com>
2023-12-26 19:51:54 +0900

    [Gold III] Title: 오등큰수, Time: 2480 ms, Memory: 343036 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -2,35 +2,38 @@ import java.util.*;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int tc = Integer.parseInt(sc.nextLine());
-    int[] input = new int[tc];
-    Map<Integer, Integer> count = new HashMap<>();
-    for(int t = 0; t < tc; t++){
-      input[t] = sc.nextInt();
-      count.put(input[t], 0);
+    int N = sc.nextInt();
+    int[] A = new int[N];
+    for(int a = 0; a < N; a++){
+      A[a] = sc.nextInt();
     }
-
-    for(int t = 0; t < tc; t++){
-      count.put(input[t], count.get(input[t]) + 1);
+    
+    Map<Integer, Integer> count = new HashMap<>();
+    for(int i = 0; i < A.length; i++){
+      if(count.containsKey(A[i])){
+        count.put(A[i], count.get(A[i]) + 1);
+        continue;
+      }
+      count.put(A[i], 1);
     }
 
     Stack<Integer> stack = new Stack<>();
-    for(int t = 0; t < tc; t++){
-      while(!stack.isEmpty() && count.get(input[stack.peek()]) < count.get(input[t])){
-        input[stack.pop()] = input[t];
+    for(int i = 0; i < A.length; i++){
+      while(!stack.isEmpty() && count.get(A[stack.peek()]) < count.get(A[i])){
+        A[stack.pop()] = A[i];
       }
-      stack.push(t);
+      stack.push(i);
     }
 
     while(!stack.isEmpty()){
-      input[stack.pop()] = -1;
+      A[stack.pop()] = -1;
     }
 
     StringBuilder sb = new StringBuilder();
-    for(int i = 0; i < input.length; i++){
-      sb.append(input[i]).append(' ');
+    for(int i = 0; i < A.length; i++){
+      sb.append(A[i]).append(' ');
     }
-
-    System.out.println(sb);
+    System.out.println(sb.toString());
+    sc.close();
   }
 }
