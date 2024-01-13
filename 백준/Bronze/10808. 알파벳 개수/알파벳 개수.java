16c1194a17bcbb9e812ee08fc7e063f7fffe675b
DoGyeong Seo <dgseo8981@gmail.com>
2024-01-13 22:42:53 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 200 ms, Memory: 17628 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,23 +1,17 @@
 import java.util.*;
 class Main {
   public static void main(String[] args) {
-    Map<Character, Integer> alphabetFrequency = new HashMap<>();
-    for(int i = 97; i < 123; i++){
-      alphabetFrequency.put((char) i, 0);
-    }
-    
     Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
-
-    for(int i = 0; i < input.length(); i++){
-      alphabetFrequency.put(input.charAt(i), alphabetFrequency.get(input.charAt(i)) + 1);
+    
+    String S = sc.next();
+    int[] count = new int[26];
+    for(int i = 0; i < S.length(); i++){
+      count[S.charAt(i) - 97] += 1;
     }
 
-    StringBuilder sb = new StringBuilder();
-    for(int i = 97; i < 123; i++){
-      sb.append(alphabetFrequency.get((char) i) + " ");
+    for(int i = 0; i < count.length; i++){
+        System.out.print(count[i] + " ");
     }
-
-    System.out.println(sb);
+    sc.close();
   }
 }
