56700e1d4215ca9bfb07657ec31e9e97e469ba22
DoGyeong Seo <dgseo8981@gmail.com>
2024-05-02 21:45:44 +0900

    [Silver III] Title: 1로 만들기, Time: 224 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,48 +1,32 @@
 import java.util.*;
-class Main {
-    private static int minCount = Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int X = sc.nextInt();
-        oper(X, 0);
-        System.out.println(minCount);
-        sc.close();
+
+public class Main {
+  static int minCount = Integer.MAX_VALUE;
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    calculate(N, 0);
+    System.out.println(minCount);
+    sc.close();
+  }
+
+  private static void calculate (int X, int count) {
+    if(X == 1){
+      minCount = Math.min(count, minCount);
+      return;
     }
-    private static void oper(int X, int count){
-        if(isOne(X)){
-            minCount = Math.min(minCount, count);
-            return;
-        }
-        if(isGreaterMinCount(minCount, count)){
-            return;
-        }
-        if(isDividableThree(X)){
-            oper(divideThree(X), count + 1);    
-        }
-        if(isDividableTwo(X)){
-            oper(divideTwo(X), count + 1);
-        }
-        oper(minusOne(X), count + 1);
+    
+    if(minCount < count){
+      return;
     }
-    private static boolean isGreaterMinCount(int minCount, int count){
-        return minCount < count;
+    
+    if(X % 3 == 0){
+      calculate(X / 3, count + 1);
     }
-    private static boolean isOne(int X){
-        return X == 1;
+    if(X % 2 == 0){
+      calculate(X / 2, count + 1);
     }
-    private static boolean isDividableTwo(int X){
-        return X % 2 == 0;
-    }
-    private static boolean isDividableThree(int X){
-        return X % 3 == 0;
-    }
-    private static int divideThree(int X) {
-        return X / 3;
-    }
-    private static int divideTwo(int X) {
-        return X / 2;
-    }
-    private static int minusOne(int X) {
-        return X - 1;
-    }
+    calculate(X - 1, count + 1);
+  }
 }
