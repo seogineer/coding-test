10104ecd499b9b3407eca97409e7ca3389e73f0f
DoGyeong Seo <dgseo8981@gmail.com>
2024-05-13 17:12:20 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 208 ms, Memory: 17756 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Main {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
@@ -7,7 +7,7 @@ class Main {
         dp[1] = 1;
         dp[2] = 3;
         for(int i = 3; i <= n; i++){
-            dp[i] = (dp[i - 1] + 2 * dp[i - 2]) % 10007;
+          dp[i] = (dp[i - 2] * 2 + dp[i - 1]) % 10007;
         }
         System.out.println(dp[n]);
         sc.close();
