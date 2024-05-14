fcd57567ca5038950193d7a47992d693922fc506
DoGyeong Seo <dgseo8981@gmail.com>
2024-05-14 22:42:06 +0900

    [Silver I] Title: 카드 구매하기, Time: 304 ms, Memory: 20988 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -1,22 +1,22 @@
 import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        int[] P = new int[N + 1];
-        for(int p = 1; p <= N; p++){
-            P[p] = sc.nextInt();
-        }
 
-        int[] maxPrice = new int[N + 1];
-        for(int i = 1; i <= N; i++){
-            for(int j = 1; j <= i; j++){
-                maxPrice[i] = Math.max(maxPrice[i], maxPrice[i - j] + P[j]);
-            }
-        }
-        
-        System.out.println(maxPrice[N]);
-        sc.close();
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] P = new int[N + 1];
+    for(int i = 1; i <= N; i++){
+      P[i] = sc.nextInt();
     }
+
+    int[] D = new int[N + 1];
+    for(int i = 1; i <= N; i++){
+      for(int j = 1; j <= i; j++){
+        D[i] = Math.max(D[i], D[i - j] + P[j]);
+      }
+    }
+
+    System.out.println(D[N]);
+    sc.close();
+  }
 }
