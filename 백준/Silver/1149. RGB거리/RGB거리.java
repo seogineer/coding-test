8cf1da86b4a1cfbd8d178f4cfb8e021c453513b1
DoGyeong Seo <dgseo8981@gmail.com>
2024-05-22 21:44:21 +0900

    [Silver I] Title: RGB거리, Time: 264 ms, Memory: 20148 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,24 +1,24 @@
 import java.util.*;
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        
-        int[][] price = new int[N][3];
-        
-        for(int i = 0; i < price.length; i++){
-            for(int j = 0; j < 3; j++){
-                price[i][j] = sc.nextInt();
-            }
-        }
-        
-        for(int i = 1; i < N; i++){
-            price[i][0] += Math.min(price[i - 1][1], price[i - 1][2]);
-            price[i][1] += Math.min(price[i - 1][0], price[i - 1][2]);
-            price[i][2] += Math.min(price[i - 1][0], price[i - 1][1]);
-        }
-
-        System.out.println(Math.min(price[N - 1][0], Math.min(price[N - 1][1], price[N - 1][2])));
-        sc.close();
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[][] price = new int[N + 1][3];
+    for(int i = 1; i < price.length; i++){
+      for(int j = 0; j < price[0].length; j++){
+        price[i][j] = sc.nextInt();
+      }
     }
+    int[][] dp = new int[N + 1][3];
+    dp[1][0] = price[1][0];
+    dp[1][1] = price[1][1];
+    dp[1][2] = price[1][2];
+    for(int i = 2; i < price.length; i++){
+      dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + price[i][0];
+      dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + price[i][1];
+      dp[i][2] = Math.min(dp[i - 1][0], dp[i - 1][1]) + price[i][2];
+    }
+    System.out.println(Math.min(dp[N][0], Math.min(dp[N][1], dp[N][2])));
+    sc.close();
+  }
 }
