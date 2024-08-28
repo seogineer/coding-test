6d274f16de9da2e5ff2cfc5dd2f064a51b4ce5a2
DoGyeong Seo <dgseo8981@gmail.com>
2024-08-28 17:55:11 +0900

    [Silver I] Title: 그림, Time: 1008 ms, Memory: 228556 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,78 +1,71 @@
 import java.util.*;
 public class Main {
-  public static final int[] dx = new int[]{1, -1, 0, 0};
-  public static final int[] dy = new int[]{0, 0, -1, 1};
-  public static int n;
-  public static int m;
-  public static int area = 0;
-  public static int count = 0;
-  public static int[][] arr;
-  public static boolean[][] isVisit;
-  public static Queue<Coordinate> q = new LinkedList<Coordinate>();
-
+  static int n;
+  static int m;
+  static int[] dx = {1, -1, 0, 0};
+  static int[] dy = {0, 0, 1, -1};
+  static int[][] map;
+  static boolean[][] visit;
+  static int count = 0;
+  static int maxArea = 0;
+  static Queue<Coordinate> queue = new LinkedList<>();
+  
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     n = sc.nextInt();
     m = sc.nextInt();
-    arr = new int[n][m];
-    isVisit = new boolean[n][m];
+    map = new int[n][m];
+    visit = new boolean[n][m];
 
     for (int i = 0; i < n; i++) {
       for (int j = 0; j < m; j++) {
-        arr[i][j] = sc.nextInt();
+        map[i][j] = sc.nextInt();
       }
     }
 
-    for (int y = 0; y < n; y++) {
-      for (int x = 0; x < m; x++) {
-        if (arr[y][x] == 1 && !isVisit[y][x]) {
-          isVisit[y][x] = true;
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        if (map[i][j] == 1 && !visit[i][j]) {
           count++;
-          bfs(x, y);
+          visit[i][j] = true;
+          bfs(j, i);
         }
       }
     }
-    
+
     System.out.println(count);
-    System.out.println(area);
+    System.out.println(maxArea);
     sc.close();
   }
 
-  public static void bfs(int x, int y) {
-    int localArea = 1;
-    q.add(new Coordinate(x, y));
-    while (!q.isEmpty()) {
-      Coordinate coordinate = q.poll();
-      for (int i = 0; i < 4; i++) {
-        int coX = coordinate.getX() + dx[i];
-        int coY = coordinate.getY() + dy[i];
-        if (0 <= coX && m > coX && 0 <= coY && n > coY) {
-          if (arr[coY][coX] == 1 && !isVisit[coY][coX]) {
-            localArea++;
-            isVisit[coY][coX] = true;
-            q.add(new Coordinate(coX, coY));
+  static void bfs(int x, int y) {
+    int area = 1;
+    queue.add(new Coordinate(x, y));
+    while(!queue.isEmpty()) {
+      Coordinate coordinate = queue.poll();
+      int coordinateX = coordinate.x;
+      int coordinateY = coordinate.y;
+      for(int i = 0; i < 4; i++) {
+        int nx = coordinateX + dx[i];
+        int ny = coordinateY + dy[i];
+        if (nx >= 0 && nx < m && ny >= 0 && ny < n) {
+          if (map[ny][nx] == 1 && !visit[ny][nx]) {
+            area++;
+            visit[ny][nx] = true;
+            queue.add(new Coordinate(nx, ny));
           }
         }
       }
     }
-    area = Math.max(area, localArea);
+    maxArea = Math.max(maxArea, area);
   }
 }
 
 class Coordinate {
-  private int x;
-  private int y;
-
-  public Coordinate(int x, int y) {
+  int x;
+  int y;
+  protected Coordinate (int x, int y) {
     this.x = x;
     this.y = y;
-  }
-
-  public int getX () {
-    return x;
-  }
-
-  public int getY() {
-    return y;
   }
 }
