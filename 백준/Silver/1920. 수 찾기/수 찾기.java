796cbe8702b89b6363d186fc647f701c4afa5dd1
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-01 22:45:20 +0900

    [Silver IV] Title: 수 찾기, Time: 1664 ms, Memory: 185848 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,43 +1,45 @@
 import java.util.*;
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int[] A = new int[N];
-    for (int n = 0; n < N; n++) {
-      A[n] = sc.nextInt();
-    }
-    
-    Arrays.sort(A);
-    
-    int M = sc.nextInt();
-    int[] targets = new int[M];
-    for (int m = 0; m < M; m++) {
-      targets[m] = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = sc.nextInt();
+        }
+
+        Arrays.sort(A);
+
+        int M = sc.nextInt();
+        for (int i = 0; i < M; i++) {
+            int target = sc.nextInt();
+            if (binarySearch(A, target)) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+
+        sc.close();
     }
 
-    for (int i = 0; i < M; i++) {
-      int target = targets[i];
-      int start = 0;
-      int end = N - 1;
-      while (true) {
-        if (start == end) {
-          if (A[start] == target) {
-            System.out.println(1);
-          } else {
-            System.out.println(0);
-          }
-          break;
+    static boolean binarySearch(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
-        int mid = (start + end) / 2;
-        if (A[mid] < target) {
-          start = mid + 1;
-        } else {
-          end = mid;
-        }
-      }
+
+        return false;
     }
-    
-    sc.close();
-  }
 }
