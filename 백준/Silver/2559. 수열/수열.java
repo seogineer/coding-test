0fdda271e48bce5ae36cde9c5d66e00ea80b7a8b
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-01 14:58:23 +0900

    [Silver III] Title: 수열, Time: 1464 ms, Memory: 113732 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,27 +1,25 @@
 import java.util.*;
+
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int K = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
 
-    int[] temperature = new int[N];
-    for (int n = 0; n < N; n++) {
-      temperature[n] = sc.nextInt();
+        int max = Integer.MIN_VALUE;
+        for (int n = 0; n <= N - K; n++) {
+            int temp = 0;
+            for (int k = n; k < n + K; k++) {
+                temp += arr[k];
+            }
+            max = Math.max(max, temp);
+        }
+
+        System.out.println(max);
+        sc.close();
     }
-
-    int max = Integer.MIN_VALUE;
-    for (int i = 0; i <= (N - K); i++) {
-      int sum = 0;
-      for (int j = i; j < (i + K); j++) {
-        sum += temperature[j];
-      }
-      if (max < sum) {
-        max = sum;
-      }
-    }
-
-    System.out.println(max);
-    sc.close();
-  }
 }
