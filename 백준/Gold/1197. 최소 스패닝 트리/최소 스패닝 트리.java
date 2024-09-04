9e7a8861cae25da2bdadf04b6917a3cef0ef9217
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-04 18:39:38 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1260 ms, Memory: 253696 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,64 +1,64 @@
 import java.util.*;
-
-class Edge implements Comparable<Edge>{
-  int to;
-  int weight;
-  
-  public Edge(int to, int weight) {
-    this.to = to;
-    this.weight = weight;
-  }
-
-  @Override
-  public int compareTo(Edge other) {
-    return Integer.compare(this.weight, other.weight);
-  }
-}
-
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    static class Edge implements Comparable<Edge> {
+        int to;
+        int weight;
 
-    int V = sc.nextInt();
-    int E = sc.nextInt();
-
-    List<ArrayList<Edge>> edges = new ArrayList<>();
-    for (int v = 0; v <= V; v++) {
-      edges.add(new ArrayList<Edge>());
-    }
-    boolean[] visit = new boolean[V + 1];
-
-    for (int e = 1; e <= E; e++) {
-      int A = sc.nextInt();
-      int B = sc.nextInt();
-      int C = sc.nextInt();
-      edges.get(A).add(new Edge(B, C));
-      edges.get(B).add(new Edge(A, C));
-    }
-
-    PriorityQueue<Edge> pq = new PriorityQueue<>();
-    pq.add(new Edge(1, 0));
-
-    int mstWeight = 0;
-    
-    while (!pq.isEmpty()) {
-      Edge current = pq.poll();
-
-      if (visit[current.to]) {
-        continue;
-      }
-
-      visit[current.to] = true;
-      mstWeight += current.weight;
-
-      for (Edge edge : edges.get(current.to)) {
-        if (!visit[edge.to]) {
-          pq.add(edge);
+        public Edge (int to, int weight) {
+            this.to = to;
+            this.weight = weight;
         }
-      }
+
+        @Override
+        public int compareTo(Edge other) {
+            return Integer.compare(this.weight, other.weight);
+        }
     }
 
-    System.out.println(mstWeight);
-    sc.close();
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+
+        List<Edge>[] edges = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
+            edges[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int weight = sc.nextInt();
+            edges[u].add(new Edge(v, weight));
+            edges[v].add(new Edge(u, weight));
+        }
+
+        boolean[] visited = new boolean[V + 1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        
+        pq.add(new Edge(1, 0));
+
+        int mstWeight = 0;
+        
+        while (!pq.isEmpty()) {
+            Edge current = pq.poll();
+
+            if (visited[current.to]) {
+                continue;
+            }
+            
+            visited[current.to] = true;
+            mstWeight += current.weight;
+            
+            for (Edge edge : edges[current.to]) {
+                if (!visited[edge.to]) {
+                    pq.add(edge);
+                }
+            }
+        }
+
+        System.out.println(mstWeight);
+        sc.close();
+    }
 }
