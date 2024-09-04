bfaea23da31aac2c8194d1437f56ee35c280314a
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-04 21:14:14 +0900

    [Gold IV] Title: 최단경로, Time: 1736 ms, Memory: 323748 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,10 +1,10 @@
 import java.util.*;
-
 public class Main {
     static class Edge implements Comparable<Edge> {
-        int to, weight;
+        int to;
+        int weight;
 
-        public Edge(int to, int weight) {
+        public Edge (int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
@@ -17,53 +17,48 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int V = sc.nextInt(); // 정점의 수
-        int E = sc.nextInt(); // 간선의 수
-        int start = sc.nextInt(); // 시작 정점
-        
-        // 인접 리스트를 사용하여 그래프를 표현
-        List<Edge>[] adj = new ArrayList[V + 1];
+
+        int V = sc.nextInt();
+        int E = sc.nextInt();
+        int start = sc.nextInt();
+
+        List<Edge>[] edges = new ArrayList[V + 1];
         for (int i = 1; i <= V; i++) {
-            adj[i] = new ArrayList<>();
+            edges[i] = new ArrayList<>();
         }
-        
-        for (int i = 0; i < E; i++) {
-            int u = sc.nextInt();
-            int v = sc.nextInt();
-            int weight = sc.nextInt();
-            adj[u].add(new Edge(v, weight));
-        }
-        
-        // 다익스트라 알고리즘을 사용하여 최단 경로 계산
+
         int[] dist = new int[V + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[start] = 0;
-        
+
+        for (int i = 0; i < E; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            int w = sc.nextInt();
+            edges[u].add(new Edge(v, w));
+        }
+
         PriorityQueue<Edge> pq = new PriorityQueue<>();
         pq.add(new Edge(start, 0));
-        
+
         while (!pq.isEmpty()) {
             Edge current = pq.poll();
-            int currentVertex = current.to;
-            int currentWeight = current.weight;
-            
-            if (currentWeight > dist[currentVertex]) {
+
+            if (dist[current.to] < current.weight) {
                 continue;
             }
-            
-            for (Edge edge : adj[currentVertex]) {
-                int nextVertex = edge.to;
-                int newDist = dist[currentVertex] + edge.weight;
-                
-                if (newDist < dist[nextVertex]) {
-                    dist[nextVertex] = newDist;
-                    pq.add(new Edge(nextVertex, newDist));
+
+            for (Edge edge : edges[current.to]) {
+                int nextTo = edge.to;
+                int nextWeight = edge.weight + dist[current.to];
+
+                if (dist[nextTo] > nextWeight) {
+                    dist[nextTo] = nextWeight;
+                    pq.add(new Edge(nextTo, nextWeight));
                 }
             }
         }
-        
-        // 출력
+
         for (int i = 1; i <= V; i++) {
             if (dist[i] == Integer.MAX_VALUE) {
                 System.out.println("INF");
