fa53f853f490e49812522a681d2776507b5b85f8
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-06 18:59:23 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 1260 ms, Memory: 250904 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,7 +4,7 @@ public class Main {
         int to;
         int weight;
 
-        public Edge (int to, int weight) {
+        public Edge(int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
@@ -21,37 +21,35 @@ public class Main {
         int V = sc.nextInt();
         int E = sc.nextInt();
 
-        List<Edge>[] edges = new ArrayList[V + 1];
+        List<Edge>[] adj = new ArrayList[V + 1];
         for (int i = 1; i <= V; i++) {
-            edges[i] = new ArrayList<>();
+            adj[i] = new ArrayList<>();
         }
 
         for (int i = 0; i < E; i++) {
             int u = sc.nextInt();
             int v = sc.nextInt();
-            int weight = sc.nextInt();
-            edges[u].add(new Edge(v, weight));
-            edges[v].add(new Edge(u, weight));
+            int w = sc.nextInt();
+            adj[u].add(new Edge(v, w));
+            adj[v].add(new Edge(u, w));
         }
 
         boolean[] visited = new boolean[V + 1];
         PriorityQueue<Edge> pq = new PriorityQueue<>();
-        
         pq.add(new Edge(1, 0));
 
         int mstWeight = 0;
-        
         while (!pq.isEmpty()) {
             Edge current = pq.poll();
 
             if (visited[current.to]) {
                 continue;
             }
-            
+
             visited[current.to] = true;
             mstWeight += current.weight;
             
-            for (Edge edge : edges[current.to]) {
+            for (Edge edge : adj[current.to]) {
                 if (!visited[edge.to]) {
                     pq.add(edge);
                 }
