6eefa85a41f7d7db7e0b0b53ff27bb30df7eae69
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-08 13:34:47 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17836 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -4,49 +4,53 @@ public class Main {
     static int[][] map;
     static boolean[][] visited;
     static int[] dy = {0, 1, 0, -1};
-    static int[] dx = {1, 0, -1, 0};
-    static int count = 0;
-    static List<Integer> counts = new ArrayList<>();
+    static int[] dx = {-1, 0, 1, 0};
+    static int count;
+    static List<Integer> result = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         N = sc.nextInt();
+
         map = new int[N][N];
-        visited = new boolean[N][N];
-        for (int j = 0; j < N; j++) {
+        for (int i = 0; i < N; i++) {
             String[] input = sc.next().split("");
-            for (int i = 0; i < N; i++) {
-                map[j][i] = Integer.parseInt(input[i]);
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(input[j]);
             }
         }
-        for (int j = 0; j < N; j++) {
-            for (int i = 0; i < N; i++) {
-                if (map[j][i] == 1 && !visited[j][i]) {
-                    count++;
-                    visited[j][i] = true;
-                    dfs(i, j);
-                    counts.add(count);
+
+        visited = new boolean[N][N];
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if(map[i][j] == 1 && !visited[i][j]) {
+                    visited[i][j] = true;
                     count = 0;
+                    dfs(i, j);
+                    result.add(count);
                 }
             }
         }
-        System.out.println(counts.size());
-        Collections.sort(counts);
-        for (int count : counts) {
-            System.out.println(count);
+
+        System.out.println(result.size());
+        Collections.sort(result);
+        for(int value : result) {
+            System.out.println(value);
         }
         sc.close();
     }
 
-    static void dfs(int x, int y) {
+    static void dfs(int y, int x) {
+        count++;
+
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny >= 0 && ny < N && nx >= 0 && nx < N) {
+            if(ny >= 0 && ny < N && nx >= 0 && nx < N) {
                 if (map[ny][nx] == 1 && !visited[ny][nx]) {
-                    count++;
                     visited[ny][nx] = true;
-                    dfs(nx, ny);
+                    dfs(ny, nx);
                 }
             }
         }
