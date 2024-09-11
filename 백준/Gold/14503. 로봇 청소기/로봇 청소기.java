83f34f9402e733bc4e4a7900d688f8c3999cae2c
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-11 18:39:34 +0900

    [Gold V] Title: 로봇 청소기, Time: 216 ms, Memory: 20676 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,60 +1,64 @@
 import java.util.*;
 public class Main {
-  static int[] dy = {-1, 0, 1, 0};
-  static int[] dx = {0, 1, 0, -1};
-  static int count = 0;
-  
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int N = sc.nextInt();
-    int M = sc.nextInt();
-    int y = sc.nextInt();
-    int x = sc.nextInt();
-    int d = sc.nextInt();
-    
-    int[][] map = new int[N][M];
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-        map[i][j] = sc.nextInt();
-      }
-    }
-    
-    while (true) {
-      if (map[y][x] == 0) {
-        map[y][x] = 2;
-        count++;
-      }
-      boolean sw = false;
-      
-      for (int i = 0; i < 4; i++) {
-        d = (d + 3) % 4;
-        int nx = x + dx[d];
-        int ny = y + dy[d];
-        
-        if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-          if (map[ny][nx] == 0) {
-            y = ny;
-            x = nx;
-            sw = true;
-            break;
-          }
-        }
-      }
+    static int count = 0;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
-      if (!sw) {
-        int nx = x - dx[d];
-        int ny = y - dy[d];
-        
-        if (map[ny][nx] == 1) {
-          break;
-        } else {
-          y = ny;
-          x = nx;
-        }
-      }
-    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
 
-    System.out.println(count);
-    sc.close();
-  }
+         int N = sc.nextInt();
+         int M = sc.nextInt();
+         int x = sc.nextInt();
+         int y = sc.nextInt();
+         int d = sc.nextInt();
+
+         int[][] map = new int[N][M];
+
+         for(int i = 0; i < N; i++) {
+             for (int j = 0; j < M; j++) {
+                 map[i][j] = sc.nextInt();
+             }
+         }
+
+         while(true) {
+             if(map[x][y] == 0) {
+                 count++;
+                 map[x][y] = 2;
+             }
+
+             boolean moved = false;
+
+             for(int i = 0; i < 4; i++) {
+                 d = (d + 3) % 4;
+                 int nx = x + dx[d];
+                 int ny = y + dy[d];
+                 if (ny >= 0 && ny < M && nx >= 0 && nx < N) {
+                     if (map[nx][ny] == 0) {
+                         moved = true;
+                         x = nx;
+                         y = ny;
+                         break;
+                     }
+                 }
+             }
+
+             if (!moved) {
+                 int bd = (d + 2) % 4;
+                 int bx = x + dx[bd];
+                 int by = y + dy[bd];
+                 if (by >= 0 && by < M && bx >= 0 && bx < N) {
+                     if (map[bx][by] == 1) {
+                         break;
+                     } else {
+                         x = bx;
+                         y = by;
+                     }
+                 }
+             }
+         }
+
+         System.out.println(count);
+         sc.close();
+    }
 }
