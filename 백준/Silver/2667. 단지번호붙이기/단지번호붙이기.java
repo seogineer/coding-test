f29f5835f8f72557eca215716b5efa4f4c07d466
DoGyeong Seo <dgseo8981@gmail.com>
2024-09-25 22:16:14 +0900

    [Silver I] Title: 단지번호붙이기, Time: 180 ms, Memory: 17804 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,19 +1,20 @@
 import java.util.*;
 public class Main {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static List<Integer> result = new ArrayList<>();
     static int N;
     static int[][] map;
     static boolean[][] visited;
-    static int[] dy = {0, 1, 0, -1};
-    static int[] dx = {-1, 0, 1, 0};
     static int count;
-    static List<Integer> result = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         N = sc.nextInt();
-
         map = new int[N][N];
+        visited = new boolean[N][N];
+
         for (int i = 0; i < N; i++) {
             String[] input = sc.next().split("");
             for (int j = 0; j < N; j++) {
@@ -21,11 +22,9 @@ public class Main {
             }
         }
 
-        visited = new boolean[N][N];
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if(map[i][j] == 1 && !visited[i][j]) {
-                    visited[i][j] = true;
+                if (map[i][j] == 1 && !visited[i][j]) {
                     count = 0;
                     dfs(i, j);
                     result.add(count);
@@ -35,22 +34,23 @@ public class Main {
 
         System.out.println(result.size());
         Collections.sort(result);
-        for(int value : result) {
-            System.out.println(value);
+        for (int complexSize : result) {
+            System.out.println(complexSize);
         }
         sc.close();
     }
 
-    static void dfs(int y, int x) {
+    static void dfs(int x, int y) {
         count++;
+        visited[x][y] = true;
 
         for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
             int nx = x + dx[i];
-            if(ny >= 0 && ny < N && nx >= 0 && nx < N) {
-                if (map[ny][nx] == 1 && !visited[ny][nx]) {
-                    visited[ny][nx] = true;
-                    dfs(ny, nx);
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
                 }
             }
         }
