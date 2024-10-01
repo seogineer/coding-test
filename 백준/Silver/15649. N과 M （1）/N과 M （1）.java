7a966c34e5686d1585d406588875ee74deecaf23
DoGyeong Seo <dgseo8981@gmail.com>
2024-10-01 09:31:15 +0900

    [Silver III] Title: N과 M (1), Time: 1592 ms, Memory: 59840 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,36 +1,40 @@
 import java.util.*;
 public class Main {
-  static int N;
-  static int M;
-  static int[] result;
-  static boolean[] visit;
-
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    N = sc.nextInt();
-    M = sc.nextInt();
-    result = new int[M];
-    visit = new boolean[N];
-    recur(0);
-    sc.close();
-  }
-
-  static void recur(int depth) {
-    if (depth == M) {
-      for (int val : result) {
-        System.out.print(val + " ");
-      }
-      System.out.println();
-      return;
+    static int N;
+    static int M;
+    static int[] selected;
+    static boolean[] used;
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        selected = new int[M];
+        used = new boolean[N + 1];
+        
+        backTrack(0);
+        
+        sc.close();
     }
-
-    for (int n = 0; n < N; n++) {
-      if (!visit[n]) {
-        visit[n] = true;
-        result[depth] = n + 1;
-        recur(depth + 1);
-        visit[n] = false;
-      }
+    
+    static void backTrack(int depth) {
+        if (depth == M) {
+            for(int val : selected) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+        
+        for(int i = 1; i <= N; i++) {
+            if(!used[i]) {
+                used[i] = true;
+                selected[depth] = i;
+                backTrack(depth + 1);
+                used[i] = false;
+            }
+        }
     }
-  }
 }
