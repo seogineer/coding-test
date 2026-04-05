e3da792ac78fb4f13813a4fb429b4d05152a6faa
DoGyeong Seo <dgseo8981@gmail.com>
2026-04-05 20:02:06 +0900

    [Silver IV] Title: 괄호, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,37 +1,33 @@
+import java.io.*;
 import java.util.*;
+
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for(int t = 0; t < T; t++){
-            boolean flag = isValid(sc.next());
-            if(flag){
-                System.out.println("YES");
-            } else {
-                System.out.println("NO");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        
+        for (int t = 0; t < tc; t++) {
+            Stack<Character> stack = new Stack<>();
+            String ps = br.readLine();
+            boolean flag = true;
+            
+            for (int i = 0; i < ps.length(); i++) {
+                char c = ps.charAt(i);
+                
+                if (c == '(') {
+                    stack.push(c);
+                    continue;
+                } else if (c == ')' && !stack.isEmpty()) {
+                    stack.pop();
+                    continue;
+                } else if (c == ')' && stack.isEmpty()) {
+                    flag = false;
+                    break;
+                }
             }
+            
+            System.out.println(stack.isEmpty() && flag ? "YES" : "NO");
         }
-        sc.close();
-    }
-
-    public static boolean isValid(String input){
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < input.length(); i++){
-            if(input.charAt(i) == '('){
-                stack.push(input.charAt(i));
-                continue;
-            } else if(input.charAt(i) == ')' && !stack.isEmpty()){
-                stack.pop();
-                continue;
-            } else if(input.charAt(i) == ')' && stack.isEmpty()){
-                return false;
-            }
-        }
-
-        if(stack.isEmpty()){
-            return true;
-        } else {
-            return false;
-        }
+        br.close();
     }
 }
