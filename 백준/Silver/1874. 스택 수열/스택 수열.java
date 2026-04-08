9067ef50577caa0721581c0920cbcad3bbc718be
DoGyeong Seo <dgseo8981@gmail.com>
2026-04-08 23:04:04 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 28148 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,28 +1,34 @@
+import java.io.*;
 import java.util.*;
+
 class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        Stack<Integer> stack = new Stack<>();
-        int start = 0;
-        for(int i = 1; i <= n; i++){
-            int input = sc.nextInt();
-            if(start < input){
-                for(int j = start + 1; j <= input; j++){
-                    stack.push(j);
-                    sb.append('+').append('\n');
+        
+        Stack<Integer> stack = new Stack();
+        int num = 1;
+        
+        for (int t = 0; t < n; t++) {
+            int input = Integer.parseInt(br.readLine());
+            
+            if (input >= num) {
+                for (int i = num; i <= input; i++) {
+                    stack.push(i);
+                    num++;
+                    sb.append("+").append("\n");
                 }
-                start = input;
-            } else if(stack.peek() != input){
+            } else if (!stack.isEmpty() && stack.peek() != input) {
                 System.out.println("NO");
                 return;
             }
-
+            
             stack.pop();
-            sb.append('-').append('\n');
+            sb.append("-").append("\n");
         }
-        System.out.println(sb.toString());
-        sc.close();
+        
+        System.out.println(sb);
+        br.close();
     }
 }
