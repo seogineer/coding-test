c7833d36bd0cb2b1c2d4a361badc44705b69a258
DoGyeong Seo <dgseo8981@gmail.com>
2026-04-12 17:55:22 +0900

    [Silver II] Title: 쇠막대기, Time: 156 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,24 +1,27 @@
+import java.io.*;
 import java.util.*;
+
 class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String input = sc.nextLine();
-    
-    int answer = 0;
-    Stack<Character> stack = new Stack<>();
-    for(int i = 0; i < input.length(); i++){
-      if(input.charAt(i) == '('){
-        stack.push(input.charAt(i));
-      } else {
-        stack.pop();
-        if(input.charAt(i - 1) == '('){
-          answer += stack.size();
-        } else {
-          answer += 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Character> stack = new Stack<>();
+        String input = br.readLine();
+        
+        int answer = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push(input.charAt(i));
+            } else {
+                stack.pop();
+                if (input.charAt(i - 1) == '(') {
+                    answer += stack.size();
+                } else {
+                    answer += 1;
+                }
+            }
         }
-      }
+        
+        System.out.println(answer);
+        br.close();
     }
-    System.out.println(answer);
-    sc.close();
-  }
 }
