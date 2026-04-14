d9ec7307ef4659f98e0908885abc1c64136655d9
DoGyeong Seo <dgseo8981@gmail.com>
2026-04-14 21:24:42 +0900

    [Gold IV] Title: 오큰수, Time: 884 ms, Memory: 144016 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -7,31 +7,30 @@ class Main {
         Stack<Integer> stack = new Stack<>();
         
         int N = Integer.parseInt(br.readLine());
-        int[] seq = new int[N];
+        int[] A = new int[N];
         
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int n = 0; n < N; n++) {
-            seq[n] = Integer.parseInt(st.nextToken());
+            A[n] = Integer.parseInt(st.nextToken());
         }
         
         for (int n = 0; n < N; n++) {
-            while (!stack.isEmpty() && seq[stack.peek()] < seq[n]) {
-                seq[stack.peek()] = seq[n];
+            while (!stack.isEmpty() && A[stack.peek()] < A[n]) {
+                A[stack.peek()] = A[n];
                 stack.pop();
             }
             stack.push(n);
         }
         
         while (!stack.isEmpty()) {
-            seq[stack.pop()] = -1;
+            A[stack.pop()] = -1;
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < seq.length; i++) {
-            sb.append(seq[i]).append(" ");
+        for (int i = 0; i < A.length; i++) {
+            sb.append(A[i]).append(" ");
         }
         System.out.println(sb);
-        
         br.close();
     }
 }
